53621140a6248e1393f5b9d7d2f13de543dec557
Barak Shirali <barak.shirali@gmail.com>
2015-06-04 10:17:18 -0400

    Push notification for GCM added

--- app/lib/notification.js
+++ app/lib/notification.js
@@ -1,19 +1,43 @@
 var db = require('../../config/sequelize');
 var _ = require('lodash');
 var async = require('async');
+var config    = require('../../config/config');
+var gcm = require('node-gcm');
 
-
-var sendGSM = function(regIds, message, next) {
+var sendGSM = function(regIds, data, next) {
+	if(regIds.length === 0) {
+		next();
+		return;
+	}
 	console.log('---Sending GSM to ' + regIds.join());
+
+	var message = new gcm.Message({
+	    collapseKey: 'RemitSystem',
+	    delayWhileIdle: true,
+	    timeToLive: 3,
+	    data: data
+	});
+
+	var sender = new gcm.Sender(config.google.API_KEY);
+
+	sender.send(message, regIds, function (err, result) {
+		if(err) console.error(err);
+		else    console.log(result);
+	});
+
 	next();
 };
 
-var sendAPN = function(tokens, message, next) {
+var sendAPN = function(tokens, data, next) {
+	if(tokens.length === 0) {
+		next();
+		return;
+	}
 	console.log('---Sending APN to ' + tokens.join());
 	next();
 };
 
-exports.sendNotification = function(userId, message, next) {
+exports.sendNotification = function(userId, data, next) {
 	db.UserDevice.all({where: {UserId: userId}})
 		.success(function(devices) {
 			var android = [], ios = [];
@@ -26,8 +50,8 @@ exports.sendNotification = function(userId, message, next) {
 				}
 			});
 
-			sendGSM(android, message, function() {
-				sendAPN(ios, message, function() {
+			sendGSM(android, data, function() {
+				sendAPN(ios, data, function() {
 					next();
 				});
 			});
